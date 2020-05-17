a049f50fda3ffe8cdc4ef178feecbe09ed076078
daichihasegawa0923 <punyachi@yahoo.co.jp>
2020-05-17 22:27:19 +0900

    タイトル画面を追加

--- CircleShooting_Game/Assets/Code/Spinner/SpinnerController.cs
+++ CircleShooting_Game/Assets/Code/Spinner/SpinnerController.cs
@@ -2,63 +2,83 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpinnerController : MonoBehaviour
+namespace DiamondGames.CicleShooting.Spinner
 {
-    [SerializeField]
-    protected GameObject _spinObject;
-
-    protected Vector3 _firstTouchScreenPosition = Vector3.zero;
-    protected bool _isTouching = false;
-
-    protected Vector3 _currentSpinObjectSpin;
-
-    [SerializeField] CanvasForDebug _canvasForDebug;
-    
-    // Start is called before the first frame update
-    void Start()
+    public class SpinnerController : MonoBehaviour
     {
-        
-    }
+        [SerializeField]
+        protected GameObject _spinObject;
 
-    // Update is called once per frame
-    protected virtual void Update()
-    {
-        Spin();
-    }
+        protected Vector3 _firstTouchScreenPosition = Vector3.zero;
+        protected bool _isTouching = false;
 
-    protected virtual void Spin()
-    {
-        if (Input.GetMouseButtonDown(0))
+        protected Vector3 _currentSpinObjectSpin;
+        protected float _touchingSpin;
+
+        [SerializeField] CanvasForDebug _canvasForDebug;
+
+        protected Vector3 _centerOfScreenPosition;
+
+        // Start is called before the first frame update
+        void Start()
         {
-            _firstTouchScreenPosition = Input.mousePosition;
-            _currentSpinObjectSpin = _spinObject.transform.eulerAngles;
-            _isTouching = true;
+            // スクリーンの中央座標の取得
+            this._centerOfScreenPosition = Vector3.zero;
+            this._centerOfScreenPosition.x = Screen.width / 2;
+            this._centerOfScreenPosition.y = Screen.height / 2;
         }
 
-        if (Input.GetMouseButtonUp(0))
+        // Update is called once per frame
+        protected virtual void Update()
         {
-            _isTouching = false;
+            this.Spin();
         }
 
-        if (!_isTouching)
-            return;
+        /// <summary>
+        /// オブジェクトを回転させる
+        /// </summary>
+        protected virtual void Spin()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                _firstTouchScreenPosition = Input.mousePosition;
+                _currentSpinObjectSpin = _spinObject.transform.eulerAngles;
+                _isTouching = true;
 
-        // スクリーンの中央座標の取得
-        var centerOfScreen = Vector3.zero;
-        centerOfScreen.x = Screen.width / 2;
-        centerOfScreen.y = Screen.height / 2;
+                _touchingSpin = this.CalcAngle(this._centerOfScreenPosition, Input.mousePosition);
+            }
 
-        // スライドした際の、中央の角度を求める
-        var firstVector = _firstTouchScreenPosition - centerOfScreen;
-        var secondVector = Input.mousePosition - centerOfScreen;
+            if (Input.GetMouseButtonUp(0))
+            {
+                _isTouching = false;
+            }
 
-        var angle = Vector3.SignedAngle(secondVector,firstVector,Vector3.up);
+            // タップ中の処理
+            if (!_isTouching)
+                return;
 
-        _canvasForDebug.SetAngleText(angle.ToString());
+            var angle = this.CalcAngle(this._centerOfScreenPosition, Input.mousePosition);
+            var reangle = angle - _touchingSpin;
+            _canvasForDebug.SetAngleText(angle.ToString() + ":" + this._touchingSpin);
 
-        //角度をオブジェクトに反映する
-        var spins = _spinObject.transform.eulerAngles;
-        spins.y = _currentSpinObjectSpin.y + angle;
-        _spinObject.transform.eulerAngles = spins;
+            //角度をオブジェクトに反映する
+            var spins = _spinObject.transform.eulerAngles;
+            spins.y = _currentSpinObjectSpin.y + reangle;
+            _spinObject.transform.eulerAngles = spins;
+        }
+
+        /// <summary>
+        /// 座標から内角を計算する
+        /// </summary>
+        /// <param name="from">座標1</param>
+        /// <param name="to">座標2</param>
+        /// <returns>内角</returns>
+        protected virtual float CalcAngle(Vector3 from, Vector3 to)
+        {
+            var dt = to - from;
+            var rad = Mathf.Atan2(dt.x, dt.y);
+            return rad * Mathf.Rad2Deg;
+        }
     }
 }
+
