2056230becde2cde72426f9a6a5d601d73558d5b
daichihasegawa0923 <punyachi@yahoo.co.jp>
2020-05-17 17:01:01 +0900

    とりあえず回る処理を実装

---
+++ CircleShooting_Game/Assets/Code/Debug/CanvasForDebug.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasForDebug : MonoBehaviour
+{
+    [SerializeField]
+    private Text _angleText;
+
+    public void SetAngleText(string textValue)
+    {
+        _angleText.text = textValue;
+    }
+}
---
+++ CircleShooting_Game/Assets/Code/Spinner/SpinnerController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinnerController : MonoBehaviour
+{
+    [SerializeField]
+    protected GameObject _spinObject;
+
+    protected Vector3 _firstTouchScreenPosition = Vector3.zero;
+    protected bool _isTouching = false;
+
+    protected Vector3 _currentSpinObjectSpin;
+
+    [SerializeField] CanvasForDebug _canvasForDebug;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    protected virtual void Update()
+    {
+        Spin();
+    }
+
+    protected virtual void Spin()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _firstTouchScreenPosition = Input.mousePosition;
+            _currentSpinObjectSpin = _spinObject.transform.eulerAngles;
+            _isTouching = true;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            _isTouching = false;
+        }
+
+        if (!_isTouching)
+            return;
+
+        // スクリーンの中央座標の取得
+        var centerOfScreen = Vector3.zero;
+        centerOfScreen.x = Screen.width / 2;
+        centerOfScreen.y = Screen.height / 2;
+
+        // スライドした際の、中央の角度を求める
+        var firstVector = _firstTouchScreenPosition - centerOfScreen;
+        var secondVector = Input.mousePosition - centerOfScreen;
+
+        var angle = Vector3.SignedAngle(secondVector,firstVector,Vector3.up);
+
+        _canvasForDebug.SetAngleText(angle.ToString());
+
+        //角度をオブジェクトに反映する
+        var spins = _spinObject.transform.eulerAngles;
+        spins.y = _currentSpinObjectSpin.y + angle;
+        _spinObject.transform.eulerAngles = spins;
+    }
+}
