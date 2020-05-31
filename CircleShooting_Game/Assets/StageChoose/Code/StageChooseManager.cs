1d6d4ffdc67b479a5783fa370bbcea863c1013aa
daichihasegawa0923 <punyachi@yahoo.co.jp>
2020-06-01 08:46:13 +0900

    ステージの選択画面の画像が切り替わる処理を実装

--- CircleShooting_Game/Assets/StageChoose/Code/StageChooseManager.cs
+++ CircleShooting_Game/Assets/StageChoose/Code/StageChooseManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class StageChooseManager : MonoBehaviour
 {
@@ -12,24 +13,44 @@ public class StageChooseManager : MonoBehaviour
     private int _index = 0;
 
     [SerializeField]
+    private Button _leftButton;
+    [SerializeField]
+    private Button _rightButton;
+    [SerializeField]
+    private Button _playButton;
+
+    [SerializeField]
     private SceneLoader _sceneLoader;
+
+    [SerializeField]
+    private Image _stageImage;
 
     public StageSelectElement GetStageSelectElement()
     {
         return this._stageSelectElements[this._index];
     }
 
+    private void OnEnable()
+    {
+        this.SetStage(0);
+    }
+
     /// <summary>
     /// 選択中のステージを変更する
     /// </summary>
     /// <param name="i">変更する数字</param>
-    public void ChangeStage(int i)
+    public void SetStage(int i)
     {
         this._index += i;
         if (this._index < 0)
             this._index = 0;
         if (this._index >= this._stageSelectElements.Length)
             this._index = this._stageSelectElements.Length - 1;
+
+        this._leftButton.interactable = this._index == 0 ? false : true;
+        this._rightButton.interactable = this._index == this._stageSelectElements.Length - 1 ? false : true;
+
+        this._stageImage.sprite = this._stageSelectElements[this._index]._stageImage;
     }
 
     public void LoadScene()
--- CircleShooting_Game/Assets/StageChoose/Code/StageSelectElement.cs
+++ CircleShooting_Game/Assets/StageChoose/Code/StageSelectElement.cs
@@ -9,4 +9,17 @@ public class StageSelectElement : MonoBehaviour
     public Sprite _stageImage;
     public string _descrioption;
     public string _highScoreKey;
+
+    public bool _isLocked = true;
+    public string _lockReleaseKeyName;
+    public int _lockReleaseScore = 0;
+    private void OnEnable()
+    {
+        if(_isLocked)
+        {
+            var score = PlayerPrefs.GetInt(this._lockReleaseKeyName,0);
+            if (score >= this._lockReleaseScore)
+                this._isLocked = false;
+        }
+    }
 }
